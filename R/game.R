cfbd77eed72f380cba83e726a449794804ac89d3
Philip Bulsink <bulsinkp@gmail.com>
2023-07-31 21:58:36 -0400

    restyled

--- R/game.R
+++ R/game.R
@@ -64,46 +64,53 @@ get_game_report <- function(season, game_id) {
 #'
 #' @return nst table after cleanup
 #' @importFrom rlang .data
-nst_table_cleanup <- function(data){
+nst_table_cleanup <- function(data) {
   data %>%
     dplyr::rename("team" = "x", "xgf" = "x_gf", "xga" = "x_ga", "xgf_percent" = "x_gf_percent") %>%
     dplyr::mutate(period = sub("\nFinal", "Final", .data$period)) %>%
     dplyr::mutate(period = sub("Final", "\nFinal", .data$period)) %>%
-    tidyr::separate_longer_delim(c("period", "toi", "cf", "cf", "ca", "cf_percent", "ff", "fa", "ff_percent", "sf", "sa",
-                                   "sf_percent", "scf", "sca", "scf_percent", "hdcf", "hdca", "hdcf_percent", "xgf",
-                                   "xga", "xgf_percent", "gf", "ga", "gf_percent"),
-                                 "\n") %>%
-    dplyr::mutate(toi_sec = time_to_sec(.data$toi),
-                  cf = as.numeric(.data$cf),
-                  ca = as.numeric(.data$ca),
-                  cf_percent = as.numeric(unlist(strsplit(.data$cf_percent, "%", fixed = TRUE)))/100,
-                  ff = as.numeric(.data$ff),
-                  fa = as.numeric(.data$fa),
-                  ff_percent = as.numeric(unlist(strsplit(.data$ff_percent, "%", fixed = TRUE)))/100,
-                  sf = as.numeric(.data$sf),
-                  sa = as.numeric(.data$sa),
-                  sf_percent = as.numeric(unlist(strsplit(.data$sf_percent, "%", fixed = TRUE)))/100,
-                  scf = as.numeric(.data$scf),
-                  sca = as.numeric(.data$sca),
-                  scf_percent = as.numeric(unlist(strsplit(.data$scf_percent, "%", fixed = TRUE)))/100,
-                  hdcf = as.numeric(.data$hdcf),
-                  hdca = as.numeric(.data$hdca),
-                  hdcf_percent = as.numeric(unlist(strsplit(.data$hdcf_percent, "%", fixed = TRUE)))/100,
-                  xgf = as.numeric(.data$xgf),
-                  xga = as.numeric(.data$xga),
-                  xgf_percent = as.numeric(unlist(strsplit(.data$xgf_percent, "%", fixed = TRUE)))/100,
-                  gf = as.numeric(.data$gf),
-                  ga = as.numeric(.data$ga),
-                  gf_percent = as.numeric(unlist(strsplit(.data$gf_percent, "%", fixed = TRUE)))/100) %>%
+    tidyr::separate_longer_delim(
+      c(
+        "period", "toi", "cf", "cf", "ca", "cf_percent", "ff", "fa", "ff_percent", "sf", "sa",
+        "sf_percent", "scf", "sca", "scf_percent", "hdcf", "hdca", "hdcf_percent", "xgf",
+        "xga", "xgf_percent", "gf", "ga", "gf_percent"
+      ),
+      "\n"
+    ) %>%
+    dplyr::mutate(
+      toi_sec = time_to_sec(.data$toi),
+      cf = as.numeric(.data$cf),
+      ca = as.numeric(.data$ca),
+      cf_percent = as.numeric(unlist(strsplit(.data$cf_percent, "%", fixed = TRUE))) / 100,
+      ff = as.numeric(.data$ff),
+      fa = as.numeric(.data$fa),
+      ff_percent = as.numeric(unlist(strsplit(.data$ff_percent, "%", fixed = TRUE))) / 100,
+      sf = as.numeric(.data$sf),
+      sa = as.numeric(.data$sa),
+      sf_percent = as.numeric(unlist(strsplit(.data$sf_percent, "%", fixed = TRUE))) / 100,
+      scf = as.numeric(.data$scf),
+      sca = as.numeric(.data$sca),
+      scf_percent = as.numeric(unlist(strsplit(.data$scf_percent, "%", fixed = TRUE))) / 100,
+      hdcf = as.numeric(.data$hdcf),
+      hdca = as.numeric(.data$hdca),
+      hdcf_percent = as.numeric(unlist(strsplit(.data$hdcf_percent, "%", fixed = TRUE))) / 100,
+      xgf = as.numeric(.data$xgf),
+      xga = as.numeric(.data$xga),
+      xgf_percent = as.numeric(unlist(strsplit(.data$xgf_percent, "%", fixed = TRUE))) / 100,
+      gf = as.numeric(.data$gf),
+      ga = as.numeric(.data$ga),
+      gf_percent = as.numeric(unlist(strsplit(.data$gf_percent, "%", fixed = TRUE))) / 100
+    ) %>%
     suppressWarnings() %>%
     return()
 }
 
-.nst_game_call <- function(season, game_id){
+.nst_game_call <- function(season, game_id) {
   nst_game_url <- glue::glue("https://www.naturalstattrick.com/game.php?season={season}&game={game}",
-                        season = season, game = game_id)
+    season = season, game = game_id
+  )
   httr::set_config(httr::user_agent("naturalstattrick r package - github.com/pbulsink/naturalstattrick"))
-  nst_html<-rvest::read_html(nst_game_url)
+  nst_html <- rvest::read_html(nst_game_url)
 
   return(nst_html)
 }
@@ -127,7 +134,7 @@ nst_short_report <- function(season, game_id) {
   game <- get_game_report(season, game_id)
 
   gall <- game$tall %>%
-    dplyr::filter(.data$period == 'Final') %>%
+    dplyr::filter(.data$period == "Final") %>%
     dplyr::select(c("team", "cf", "ff", "sf", "scf", "hdcf", "xgf", "gf")) %>%
     dplyr::mutate("h_a" = c("home", "away"))
 
